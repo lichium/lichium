585ef26f82912bcdb6900fa9408546755ff25059
Limpu <81900789+Limpu403@users.noreply.github.com>
2022-02-26 22:51:15 +0800

    Update music.py
    支持nonebot2.0.0a16

--- LiCat/music.py
+++ LiCat/music.py
@@ -1,44 +1,62 @@
 '''
-搜索音乐、随机音乐（网易云音乐）
-
-指令：（歌名可选，无歌名则返回随机音乐）
 点歌 [歌名]
+
+歌名可选，不填为随机
 '''
 from nonebot import on_command
+from nonebot.log import logger
 from nonebot.rule import to_me
-from nonebot.typing import T_State
-from nonebot.adapters import Bot, Event, cqhttp
-import requests, re
+from nonebot.matcher import Matcher
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+
+import re,httpx
 
 music = on_command("点歌", rule=to_me(), priority=5)
 
 @music.handle()
-async def handle_music_name(bot: Bot, event: Event, state: T_State):
-    args = str(event.get_message()).strip()
-    if args:
-        music_back = await get_music(args)
+async def handle(matcher: Matcher, args: Message = CommandArg()):
+    args = str(args).strip()
+    if args =='':
+        id = await random_music()
     else:
-        music_back = await random_music()    #否则随即音乐
-    await music.finish(music_back)
+        id = await search_music(args)
+    if id == '':
+        msg='[ERROR]'
+    else:
+        msg={'type':'music','data':{'type':'163','id':id}}
+    await music.finish(msg)
+    
+async def search_music(song_name:str) ->str:
+    head={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78"}
+    para={'s':song_name,'type':1}
+    api='https://music.163.com/api/search/get/web'
+    async with httpx.AsyncClient() as client:
+        try:
+            back=await client.get(url=api,params=para,headers=head)
+        except:
+            logger.error('music.163.com访问超时')
+            return ''
+    if back.status_code!=200:
+        logger.error('music.163.com状态码错误')
+        return ''
+    data=back.json()
+    if data['result']['songCount']==0:
+        return ''
+    return str(data['result']['songs'][0]['id'])
 
-async def get_music(music_name: str):
-    try:
-        music_back = requests.post('http://music.163.com/api/search/pc',data={'s':music_name,'offset':0,'limit':1,'type':1}).json()
-    except:
-        return '[ERROR]连接超时'
-    else:
-        if music_back['result']['songCount'] != 0:    #如果搜索到歌曲
-            return cqhttp.message.Message(f"[CQ:music,type=custom,url={'https://music.163.com/#/song?id='+str(music_back['result']['songs'][0]['id'])},audio={'http://music.163.com/song/media/outer/url?id='+str(music_back['result']['songs'][0]['id'])},title={music_back['result']['songs'][0]['name']},image={music_back['result']['songs'][0]['album']['blurPicUrl']+'?param=130y130'},content={music_back['result']['songs'][0]['artists'][0]['name']}]")
-        else :
-            return '没有搜索到相关歌曲QAQ'
-
-async def random_music():
-    try:
-        music_back = requests.get('https://api.uomg.com/api/rand.music?sort=热歌榜&format=json').json()
-    except:
-        return '[ERROR]连接超时'
-    else:
-        if music_back['code'] == 1:
-            return cqhttp.message.Message(f"[CQ:music,type=custom,url={music_back['data']['url'].replace('http://music.163.com/song/media/outer/url','https://music.163.com/#/song')},audio={music_back['data']['url']},title={music_back['data']['name']},image={music_back['data']['picurl']+'?param=130y130'},content={music_back['data']['artistsname']}]")
-        else:
-            return '随即歌曲失败！请重试'
+async def random_music() ->str:
+    api='https://api.uomg.com/api/rand.music?format=json'
+    async with httpx.AsyncClient() as client:
+        try:
+            back=await client.get(url=api)
+        except:
+            logger.error('api.uomg.com访问超时')
+            return ''
+    if back.status_code != 200:
+        logger.error('api.uomg.com状态码错误')
+        return ''
+    data=back.json()
+    if data['code']!=1:
+        return ''
+    return str(re.search('(?<=\?id=)\d+',data['data']['url']).group(0))
