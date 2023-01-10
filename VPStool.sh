cefa62517d2a9142ae86f7ece0608ef89be60af0
Limnium <81900789+Limnium@users.noreply.github.com>
2023-01-10 23:13:34 +0800

    Create VPStool.sh

---
+++ VPStool.sh
@@ -0,0 +1,39 @@
+#VPStool.sh
+#一些快速搭建服务器的脚本
+
+echo "1. warp - 为IPv6 only VPS添加IPv4网络接口（新机首步）「必」"
+echo "2. x-ui - 支持多用户协议的xray面板（梯子）"
+
+echo "3. 查找python，查看所有安装的版本"
+echo "4. 安装pip3"
+
+echo "5. 安装nb-cli"
+echo "6. 安装gocq插件"
+
+echo "请输入[1-6]："
+
+read cmd
+
+if [ cmd == 1 ]
+then
+wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh 
+elif [ cmd == 2 ]
+then
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+elif [ cmd == 3]
+then
+whereis python
+elif [ cmd == 4 ]
+then
+sudo apt update
+sudo apt install python3-pip
+pip3 --version
+elif [ cmd == 5 ]
+then
+pip3 install nb-cli
+elif [ cmd == 6 ]
+then
+nb plugin install nonebot-plugin-gocqhttp
+else
+echo "认真输入了吗？"
+fi 
